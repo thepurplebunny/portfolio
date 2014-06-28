6754a85b6d6dd98477e13f55f17f066b060bc782
Daniela Consti <dani@mural.ly>
2014-06-28 13:03:11 -0300

    added sidebar with animations and portfolio index

--- js/main.js
+++ js/main.js
@@ -8,8 +8,23 @@ $(document).ready(function() {
   }); 
  
   $(".menu-trigger").click(function(event) {
-  	$("nav").toggleClass('show').removeClass('hidden');
+  	$("nav").toggleClass('show');
+    $("main, section, footer").toggleClass('no-scroll').toggleClass('hide');;
   });
 
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 });

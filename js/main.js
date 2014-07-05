0838f210e7ce57e25d6334be472eef61a6ecd09d
Daniela Consti <dani@mural.ly>
2014-07-05 19:50:07 -0300

    Added iPad styles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
- 
+  
+  var owl = $(".slider");
+      
+
   $(".slider").owlCarousel({
     items : 3,
     singleItem: true,
     lazyLoad : true,
-    navigation : false
+    navigation : false,
+    addClassActive: true,
+    afterAction : afterAction
   }); 
- 
+
+  
+    function afterAction(){
+      var current = this.owl.currentItem;
+         if (current == 0) {
+                $('.latest-work').css("background", "#42c1b0");
+         } else if (current == 1) {
+                $('.latest-work').css("background", "#0571aa");
+         } else  {
+                $('.latest-work').css("background", "#ee8433");
+         }
+      }
+      
+  // Menu trigger
   $(".menu-trigger").click(function(event) {
   	$("nav").toggleClass('show');
     $("main, section, footer").toggleClass('no-scroll').toggleClass('hide');;
